880a1cdf3afa3191c28ec88e86c940ac914b8dba
new-crystal <newcrystal670@gmail.com>
2024-04-08 14:38:37 +0900

    Server

--- main/push_library/push.php
+++ main/push_library/push.php
@@ -1,13 +1,15 @@
 <?php
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
 require_once ('/var/www/icomes.or.kr/main/plugin/google-api-php-client-main/vendor/autoload.php');
 class Push
 {
     //[240214] sujeong / api key 전송 방식에서 token 전송 방식으로 수정 / device 상관 X 
     static function fcmMultiPushV2($title, $message, $device, $to_list, $data, $body_key="", $body_args=[""], $img_path=""){
 
-        $url    = 'https://fcm.googleapis.com/v1/projects/icomes2024/messages:send';
+        $url    = 'https://fcm.googleapis.com/v1/projects/icomes2023/messages:send';
 
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=/var/www/icomes.or.kr/main/push_library/icomes2024_key.json');
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=/var/www/icomes.or.kr/main/push_library/icomes2023_key.json');
         
         $scope = 'https://www.googleapis.com/auth/firebase.messaging';
         
@@ -28,10 +30,11 @@ class Push
             for($a = 0; $a < count($to_list); $a++) {
                 $fields = array(
                     'token'  => $to_list[$a],
-                    'notification'      => array('title'=> $title,'body'=> $message)
+                    'notification'  => array('title'=> $title,'body'=> $message)
                 );
 
             $message_json = array('message'=>$fields);
+            //print_r($message_json);
             self::sendAsync($url, $headers, $message_json);
             }
     }
