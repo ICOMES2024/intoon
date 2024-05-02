f3c801c3ad52ec91f17b0f165a47a4e1b521a890
new-crystal <oj3339@naver.com>
2024-05-02 13:18:48 +0900

    [240502] edit accommodation page

--- main/accommodation.php
+++ main/accommodation.php
@@ -23,8 +23,9 @@
                     <div class="btn_wrap">
                          <button class="btn green_btn"
                         onClick="javascript:window.open('https://www.hilton.com/en/hotels/selcici-conrad-seoul/')">Go to Website</button>
-                    <a href="./download/ICOMES_2023_Conrad_Seoul_Reservation_Form.doc" class="btn green_btn download_btn" target="_blank" download=""><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Reservation Form Download</a>
-                    <!-- onClick="javascript:alert('Reservation is closed.');" -->
+                    <a href="" class="btn green_btn download_btn not_yet" target="_blank"><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Reservation Form Download</a>
+                    <!-- <a href="./download/ICOMES_2023_Conrad_Seoul_Reservation_Form.doc" class="btn green_btn download_btn" target="_blank" download=""><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Reservation Form Download</a>
+                     -->
                     </div>
                 </div>
                 <div>
@@ -56,14 +57,14 @@
                                     <th>Email</th>
                                     <td colspan="3">
                                         <a href="mailto:Conrad_Seoul@conradhotels.com" target="_blank"
-                                            class="underline">Conrad_Seoul@conradhotels.com</a>
+                                            class="underline link">Conrad_Seoul@conradhotels.com</a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>Website</th>
                                     <td colspan="3">
                                         <a href="https://www.hilton.com/en/hotels/selcici-conrad-seoul/" target="_blank"
-                                            class="underline">https://www.hilton.com</a>
+                                            class="underline link">https://www.hilton.com</a>
                                     </td>
                                 </tr>
                                 <tr>
@@ -72,11 +73,11 @@
                                         1. <span class="font_b">Download the reservation form.</span><br>
                                         2. Fill out the form.<br>
                                         3. <span class="font_b">Send it directly to the hotel email</span><br>
-                                        (<a href="mailto:Conradseoul_GroupReservations@hilton.com" target="_blank" class="underline">Conradseoul_GroupReservations@hilton.com</a>)<br>
+                                        (<a href="mailto:Conradseoul_GroupReservations@hilton.com" target="_blank" class="underline link">Conradseoul_GroupReservations@hilton.com</a>)<br>
                                         4. Confirm the confirmation letter.
                                         <p class="red_t font_small mt4">
-                                            * Reservation can be changed, canceled, or amended up to 30 days before the congress date within a special rate from ICOMES 2023 (until August 6th).<br>
-                                            * Starting August 7th, the rate may vary, and it is non-refundable upon booking. For further information, please kindly contact the hotel directly.
+                                            * Reservation can be changed, canceled, or amended up to 30 days before the congress date within a special rate from ICOMES 2024.<br>
+                                            * Starting, the rate may vary, and it is non-refundable upon booking. For further information, please kindly contact the hotel directly.
                                         </p>
                                     </td>
                                </tr>
@@ -91,13 +92,12 @@
                     <div class="imgs"></div>
                     <div class="btn_wrap">
                          <button class="btn green_btn"
-                        onClick="javascript:window.open('https://www.seoulgarden.co.kr/EN/')">Go to Website</button>
-                    <a href="./download/ICOMES_2023_Seoul_Garden_Reservation_Form.docx" class="btn green_btn download_btn" target="_blank" download=""><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Reservation Form Download</a>
+                        onClick="javascript:window.open('https://www.lottehotel.com/mapo-city/en.html')">Go to Website</button>
                     <!-- onClick="javascript:alert('Reservation is closed.');" -->
                     </div>
                 </div>
                 <div>
-                    <p>Seoul Garden Hotel & Suites <span class="font_16">(Special rate for ICOMES participants applied)</span></p>
+                    <p>Lotte City Hotel Mapo</p>
                     <div class="table_wrap x_scroll">
                         <table class="c_table2 detail_table layout_fixed">
                             <colgroup>
@@ -115,27 +115,27 @@
                                 </tr>
                                 <tr>
                                     <th>Address</th>
-                                    <td colspan="3">58-Mapo-daero, Mapo-gu, Seoul, Republic of Korea</td>
+                                    <td colspan="3">109 Mapo-daero, Mapo-gu, Seoul</td>
                                 </tr>
                                 <tr>
                                     <th>Tel / Fax</th>
-                                    <td colspan="3">Tel +82-2-710-7111 / Fax +82-2-710-7333</td>
+                                    <td colspan="3">Tel +82-2-771-1004 / Fax +82-2-6009-1004</td>
                                 </tr>
                                 <tr>
                                     <th>Email</th>
                                     <td colspan="3">
-                                        <a href="mailto:resv@seoulgarden.co.kr" target="_blank"
-                                            class="underline">resv@seoulgarden.co.kr</a>
+                                        <a href="mailto:Leehd@lotte.net" target="_blank"
+                                            class="underline link">Leehd@lotte.net</a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>Website</th>
                                     <td colspan="3">
-                                        <a href="https://www.seoulgarden.co.kr/EN/" target="_blank"
-                                            class="underline">https://www.seoulgarden.co.kr</a>
+                                        <a href="https://www.lottehotel.com/mapo-city/en.html" target="_blank"
+                                            class="underline link">https://www.lottehotel.com/mapo-city/en.html</a>
                                     </td>
                                 </tr>
-                                <tr>
+                                <!-- <tr>
                                     <th>How to<br>reserve</th>
                                     <td colspan="3">
                                         1. <span class="font_b">Download the reservation form.</span><br>
@@ -145,7 +145,7 @@
                                         4. Confirm the confirmation letter.
                                         <p class="red_t font_small mt4">* Reservation must reach the hotel by 23rd August 2023.</p>
                                     </td>
-                               </tr>
+                               </tr> -->
                             </tbody>
                         </table>
                     </div>
@@ -157,13 +157,13 @@
                     <div class="imgs"></div>
                     <div class="btn_wrap">
                          <button class="btn green_btn"
-                        onClick="javascript:window.open('https://www.shillastay.com/mapo/')">Go to Website</button>
-                    <a href="./download/ICOMES_2023_Shilla_Stay_Mapo_Hotel_Reservation_Form.doc" class="btn green_btn download_btn" target="_blank" download=""><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Reservation Form Download</a>
+                        onClick="javascript:window.open('https://www.marriott.com/reservation/rateListMenu.mi')">Go to Website</button>
+                    <!-- <a href="./download/ICOMES_2023_Shilla_Stay_Mapo_Hotel_Reservation_Form.doc" class="btn green_btn download_btn" target="_blank" download=""><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Reservation Form Download</a> -->
                     <!-- onClick="javascript:alert('Reservation is closed.');" -->
                     </div>
                 </div>
                 <div>
-                    <p>Shilla Stay Mapo <span class="font_16">(Special rate for ICOMES participants applied)</span></p>
+                    <p>Fairfield by Marriott Seoul</p>
                     <div class="table_wrap x_scroll">
                         <table class="c_table2 detail_table layout_fixed">
                             <colgroup>
@@ -181,27 +181,27 @@
                                 </tr>
                                 <tr>
                                     <th>Address</th>
-                                    <td colspan="3">83-Mapo-daero, Mapo-gu, Seoul, Republic of Korea</td>
+                                    <td colspan="3">870 Gyeongin-ro, Yeongdeungpo-gu, Seoul</td>
                                 </tr>
                                 <tr>
                                     <th>Tel / Fax</th>
-                                    <td colspan="3">Tel +82-2-2230-0700 / Fax +82-2-6979-9100</td>
+                                    <td colspan="3">Tel +82-02-21089000 / Fax +82-2-2108-9001</td>
                                 </tr>
                                 <tr>
                                     <th>Email</th>
                                     <td colspan="3">
-                                        <a href="mailto:ma_po@shillastay.com" target="_blank"
-                                            class="underline">ma_po@shillastay.com</a>
+                                        <a href="mailto:ffi.selfi.rsvn@fairfieldseoul.com" target="_blank"
+                                            class="underline link">ffi.selfi.rsvn@fairfieldseoul.com</a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>Website</th>
                                     <td colspan="3">
-                                        <a href="https://www.shillastay.com/mapo/index.do" target="_blank"
-                                            class="underline">https://www.shillastay.com/mapo</a>
+                                        <a href="https://www.marriott.com/reservation/rateListMenu.mi" target="_blank"
+                                            class="underline link">https://www.marriott.com/reservation/rateListMenu.mi</a>
                                     </td>
                                 </tr>
-                                <tr>
+                                <!-- <tr>
                                     <th>How to<br>reserve</th>
                                     <td colspan="3">
                                         1. <span class="font_b">Download the reservation form.</span><br>
@@ -211,7 +211,7 @@
                                         4. Confirm the confirmation letter.
                                         <p class="red_t font_small mt4">* Reservation must reach the hotel by 23rd August 2023.</p>
                                     </td>
-                               </tr>
+                               </tr> -->
                             </tbody>
                         </table>
                     </div>
@@ -222,10 +222,10 @@
                 <div>
                     <div class="imgs"></div>
                     <button class="btn green_btn"
-                        onClick="javascript:window.location.href='https://all.accor.com/hotel/B200/index.ko.shtml'">Go to Website</button>
+                        onClick="javascript:window.location.href='https://www.shillastay.com/mapo/index.do'">Go to Website</button>
                 </div>
                 <div>
-                    <p>Fairmont Ambassador Seoul</p>
+                    <p>Shilla Stay Mapo</p>
                     <div class="table_wrap x_scroll">
                         <table class="c_table2 detail_table layout_fixed">
                             <colgroup>
@@ -243,24 +243,24 @@
                                 </tr>
                                 <tr>
                                     <th>Address</th>
-                                    <td colspan="3">108 Yeoui Daero Yeongdeungpo Gu, Seoul, 07335, Republic of Korea</td>
+                                    <td colspan="3">83 Mapo-daero, Mapo-gu, Seoul, Republic of Korea</td>
                                 </tr>
                                 <tr>
                                     <th>Tel / Fax</th>
-                                    <td colspan="3">Tel +82-2-3395-6000 / Fax +82-2-3395-6170</td>
+                                    <td colspan="3">Tel +82-2-2230-0700  / Fax +82-2-6979-9100</td>
                                 </tr>
                                 <tr>
                                     <th>Email</th>
                                     <td colspan="3">
-                                        <a href="mailto:Fairmont.Seoul@fairmont.com " target="_blank"
-                                            class="underline">Fairmont.Seoul@fairmont.com </a>
+                                        <a href="mailto:ma_po@shilllast.com" target="_blank"
+                                            class="underline link">ma_po@shilllast.com</a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>Website</th>
                                     <td colspan="3">
-                                        <a href="https://all.accor.com/hotel/B200/index.ko.shtml " target="_blank"
-                                            class="underline">https://all.accor.com</a>
+                                        <a href="https://www.shillastay.com/mapo/index.do" target="_blank"
+                                            class="underline link">https://www.shillastay.com/mapo/index.do</a>
                                     </td>
                                 </tr>
 
@@ -270,7 +270,7 @@
                 </div>
             </li>
             <!--5-->
-            <li>
+            <!-- <li>
                 <div>
                     <div class="imgs"></div>
                     <button class="btn green_btn"
@@ -315,19 +315,11 @@
                                             class="underline">https://www.glad-hotels.com</a>
                                     </td>
                                 </tr>
-                                <!-- <tr class="small_padding">
-                                    <th>Promotion<br />Code</th>
-                                    <td colspan="3" class="bold">
-                                        ICOMES
-                                        <span class="red_txt">※ When making a reservation, you must select and enter it
-                                            to apply.</span>
-                                    </td>
-                                </tr> -->
                             </tbody>
                         </table>
                     </div>
                 </div>
-            </li>
+            </li> -->
         </div>
     </div>
 </section>
