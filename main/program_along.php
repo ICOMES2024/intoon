6b8fc8f8b2938811dd76d215f9f9f9187078e6ac
new-crystal <oj3339@naver.com>
2024-05-08 14:53:25 +0900

    [240508] add along program

---
+++ main/program_along.php
@@ -0,0 +1,128 @@
+<?php
+include_once('./include/head.php');
+include_once('./include/header.php');
+
+?>
+<section class="abstract_submission_guideline container">
+    <h1 class="page_title" style="padding: 33px 0 48px;">ALONG Program<p>-ASIA Leaders Obesity Network Group-</p></h1>
+    <div class="inner">
+        <div class="section section1">
+            <div>
+                <div class="text_box">
+                    <ul>
+                        <li>ICOMES organized by KSSO(Korean Society for the Study of Obesity) offers an exceptional opportunity for international participants to gain valuable insight and advance their career in the healthcare field.</li>
+                        <li><br>Join our comprehensive program featuring situational analysis workshop, visit to healthcare system sites, and the chance to gain invaluable insights.</li>
+                        <!--
+						<li class="f_bold">• If you are selected by submitting an abstract, 100% of the registration fee can be reduced.</li>
+						<li>• The reduction is based on the payment of the pre-registration fee, and only the submitter will receive a refund within 2 weeks after the congress.</li>
+						-->
+                    </ul>
+                </div>
+            </div>
+            <!--Instructions start-->
+            <div>
+                <div class="section_title_wrap2">
+                    <h3 class="title">Information of ALONG Program</h3>
+                </div>
+                <div class="table_wrap x_scroll">
+                    <table class="c_table detail_table">
+                        <colgroup>
+                            <col width="5%">
+                            <col>
+                        </colgroup>
+                        <tr>
+                            <th class="along_th center_t bold">Goals</th>
+                            <td class="along_th" colspan="3">Develop outstanding relationships with APAC countries, and establish obesity as a priority in their individual countries</td>
+                        </tr>
+                        <tr>
+                            <th class="along_td center_t bold">Date</th>
+                            <td class="along_td" colspan="3">September 4 (Wed.) ~ 5 (Thru.), 2024</td>
+                        </tr>
+                        <tr>
+                            <th class="along_th center_t bold">Place</th>
+                            <td class="along_th" colspan="3">CONRAD Seoul Hotel</td>
+                        </tr>
+                        <tr>
+                            <th class="along_td center_t bold" rowspan="3">Program</th>
+                            <th class="bold center_t dark_along_th">Date</th>
+                            <th class="bold center_t dark_along_th">Time</th>
+                            <th class="bold center_t dark_along_th">Details</th>
+                        </tr>
+                        <tr>
+                            <td class="along_th">September, 4 (Wed.)</td>
+                            <td class="along_th">14:00~</td>
+                            <td class="along_th">
+                                <p class="bold">Workshop -Presentation and Discussion</p>
+                                - Related to the epidemiology, definition, stigmata, and public health burden of obesity
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="along_td">September, 5(Thur.)</td>
+                            <td class="along_td">08:00~</td>
+                            <td class="along_td">
+                                <p class="bold">Hospital Tour</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <!--Instructions end-->
+			<!--Originality & Eligibility start-->
+            <div>
+                <div class="section_title_wrap2">
+                    <h3 class="title">Information for Application</h3>
+                </div>
+                <div class="table_wrap  x_scroll">
+                    <table class="c_table detail_table">
+                        <colgroup>
+                            <col width="10%">
+                            <col>
+                        </colgroup>
+                        <tr>
+                            <th class="info_th center_t">Qualification</th>
+                            <td class="info_th">For APEC residents interested in ASIA Leaders Obesity Network Group</td>
+                        </tr>
+                        <tr>
+                            <th class="info_td center_t">Limit</th>
+                            <td class="info_td">18 people</td>
+                        </tr>
+                        <tr>
+                            <th class="info_th center_t">Participation Fee</th>
+                            <td class="info_th">Complimentary</td>
+                        </tr>
+                        <tr>
+                            <th class="info_td center_t">Benefit</th>
+                            <td class="info_td">Offering 1 day accommodation at CONRAD SEOUL on September 4, 2024</td>
+                        </tr>
+                        <tr>
+                            <th class="info_th center_t">Application Deadline</th>
+                            <td class="info_th">June 12, 2024 (Wed.)</td>
+                        </tr>   
+                        <tr>
+                            <th class="info_td center_t">How to Application</th>
+                            <td class="info_td">1. Download and fill out the Curriculum vitae template<br>
+                                2. Access the below link ”Apply for ALONG Program”<br>
+                                3. Complete the form and submit it along with your CV
+                            </td>
+                        </tr>   
+                        <tr>
+                            <th class="info_th">Remark</th>
+                            <td class="info_th">- This program is available to all, irrespective of ICOMES 2024 participation.<br>
+                                - Upon application, a curriculum vitae is required.<br/>
+                                - <span class="bold">Acceptance notification will be informed via personal email at <span class="point3_txt bold">the beginning of July</span></span>                
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="text_center along_btn_box mt25">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSc6pbvmim7Oi_ZQuiYNpGvws7T8ZyFYdvY-7SYEDuBlE3CNHg/viewform" target="_blank"  class="btn long_btn" style="background-color: #324d1f; border:2px solid #2b4719;border-radius:8px;line-height: 44px;">Apply for ALONG Program</a> 
+            <a href="https://image.webeon.net/icomes2024/download/ICOMES_ALONG_Program_CV_Form.docx" class="btn long_btn yellow_btn" target="_blank" download style="background-color: #a9d18e; border:2px solid #2b4719;border-radius:8px;line-height: 44px;" ><img src="https://image.webeon.net/icomes2024/logo/icon_download_white.svg" alt="">Curriculum Vitae Template</a>
+        </div>
+        <!--//section1-->
+
+    </div>
+</section>
+
+<?php include_once('./include/footer.php'); ?>
