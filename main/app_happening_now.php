b9ff9e22c3466f0db80e96a8b4589aadcd338f06
new-crystal <oj3339@naver.com>
2024-09-04 15:33:38 +0900

    [240904] edit happening now

--- main/app_happening_now.php
+++ main/app_happening_now.php
@@ -1,16 +1,19 @@
 <?php include_once('./include/head.php');?>
 <?php include_once('./include/app_header.php');?>
 <?php
-//$today= "2023-09-09";
-$today=date("Y-m-d");
+// $today = "2024-09-07";
+// $todayTime = "2024-09-07 13:55:34";
+$today = date("Y-m-d");
+$todayTime = date("Y-m-d H:i:s");
 
 $select_program_query = "
                             SELECT p.idx, program_name, program_tag_name, chairpersons, program_place_idx, pp.program_place_name ,program_date, 
                                    date_format(start_time, '%H:%i') as start_time, date_format(end_time, '%H:%i') as end_time,
+								   DATE_FORMAT(p.start_time, '%Y-%m-%d %H:%i:%s') as program_start_time, DATE_FORMAT(p.end_time, '%Y-%m-%d %H:%i:%s') as program_end_time,
                                    (CASE
-                                       WHEN program_date = '2023-09-07' THEN 'day_1'
-                                       WHEN program_date = '2023-09-08' THEN 'day_2'
-                                       WHEN program_date = '2023-09-09' THEN 'day_3'
+                                       WHEN program_date = '2024-09-05' THEN 'day_1'
+                                       WHEN program_date = '2024-09-06' THEN 'day_2'
+                                       WHEN program_date = '2024-09-07' THEN 'day_3'
                                        ELSE ''
                                        END
                                    ) as day
@@ -26,7 +29,7 @@ $program_list = get_data($select_program_query);
 
 <!-- HUBDNCAJY : App - HAPPENING NOW 페이지 -->
 <section class="container app_version app_happening app_scientific">
-	<div class="app_title_box">
+<div class="app_title_box">
 		<h2 class="app_title">
 			HAPPENING NOW
 			<button type="button" class="app_title_prev" onclick="javascript:window.location.href='./app_index.php';"><img src="https://image.webeon.net/icomes2024/app/2024_icon_arrow_prev_wh.svg" alt="이전페이지로 이동"></button>
@@ -38,26 +41,54 @@ $program_list = get_data($select_program_query);
 			<ul class="program_detail_ul">
                 <?php
                     if($program_list !== array()){
+						
+						//오늘 진행 중인 세션 모두 출력
                         foreach ($program_list as $program){
-                ?>
-				<li name="<?=$program['program_tag_name']?>" class="<?=$program['day']?>">
-					<div class="main">
-						<button class="detail_btn"></button>
-						<p class="title"><?=$program['program_name']?></p>
-                        <?php
-                            if($program['chairpersons']!==null){
-                        ?>
-						<p class="chairperson"><span class="bold">Chairpersons:</span> <?=$program['chairpersons']?></p>
-                        <?php
-                        }
-                        ?>
-						<div class="info">
-							<span class="time"><?=$program['start_time']?>-<?=$program['end_time']?></span>
-							<span class="branch"><?=$program['program_place_name']?></span>
-						</div>
-					</div>
-					<input type="hidden" name="e" value="<?=$program['program_place_name']?>">
-				</li>
+							// $str_now = date("Y-m-d H:i:s", strtotime($todayTime));
+							$str_now = date("Y-m-d H:i:s", strtotime($todayTime));
+							$str_start = date("Y-m-d H:i:s", strtotime('-10 minutes', strtotime($program['program_start_time'])));
+							$str_end = date("Y-m-d H:i:s", strtotime($program['program_end_time']));
+							
+							//[231218] sujeong 추가 / 현재 시간 진행 중 -> 파란 배경
+							if($str_start <= $str_now && $str_end >= $str_now ){ ?>
+								<li name="<?=$program['program_tag_name']?>" class="<?=$program['day']?> now_time" >
+									<div class="main">
+										<button class="detail_btn"></button>
+										<p class="title"><?=$program['program_name']?></p>
+										<?php
+											if($program['chairpersons']!==null){
+										?>
+										<p class="chairperson"><span class="bold">Chairpersons:</span> <?=$program['chairpersons']?></p>
+										<?php
+										}
+										?>
+										<div class="info">
+											<span class="time"><?=$program['start_time']?>-<?=$program['end_time']?></span>
+											<span class="branch"><?=$program['program_place_name']?></span>
+										</div>
+									</div>
+									<input type="hidden" name="e" value="<?=$program['program_place_name']?>">
+								</li>
+							<?php }else{  ?>
+								<li name="<?=$program['program_tag_name']?>" class="<?=$program['day']?>">
+									<div class="main">
+										<button class="detail_btn"></button>
+										<p class="title"><?=$program['program_name']?></p>
+										<?php
+											if($program['chairpersons']!==null){
+										?>
+										<p class="chairperson"><span class="bold">Chairpersons:</span> <?=$program['chairpersons']?></p>
+										<?php
+										}
+										?>
+										<div class="info">
+											<span class="time"><?=$program['start_time']?>-<?=$program['end_time']?></span>
+											<span class="branch"><?=$program['program_place_name']?></span>
+										</div>
+									</div>
+									<input type="hidden" name="e" value="<?=$program['program_place_name']?>">
+								</li>
+								<?php	}?>
                 <?php
                     }
                 }
@@ -80,9 +111,6 @@ $program_list = get_data($select_program_query);
 
 <script>
 	$(document).ready(function() {
-
-        $(".app_header").removeClass("simple");  
-
 		$(".program_detail_ul > li").click(function() {
 			var e = $(this).find("input[name=e]").val();
 			var day = $(this).attr("class");
@@ -97,6 +125,30 @@ $program_list = get_data($select_program_query);
 			window.location.href = "./app_program_detail.php?day=" + day + "&e=" + e + "&name=" + this_name;
 		};
     });
+
+	const program = document.querySelector('.now_time');
+	
+	if(program){
+		focusTable(program)
+	}
+    
+
+
+	function focusTable(timeDiv) {
+    // 요소의 뷰포트 내 위치를 가져옵니다.
+    const rect = timeDiv.getBoundingClientRect();
+    const absoluteElementTop = rect.top + window.pageYOffset;
+    
+    // offset 값을 중간보다 위로 위치하도록 조정 (추가로 100px 위로 위치하게 조정)
+    const offset = (window.innerHeight / 2 - rect.height / 2) - 175; // 원하는 픽셀 값을 더하거나 빼면 됨
+
+    // 중간보다 위로 조정된 절대 위치를 계산합니다.
+    const scrollToPosition = absoluteElementTop - offset;
+
+    // 조정된 위치로 스크롤합니다.
+    window.scrollTo({ top: scrollToPosition, behavior: 'smooth' });
+}
+
 </script>
 
 <?php include_once('./include/app_footer.php');?>
